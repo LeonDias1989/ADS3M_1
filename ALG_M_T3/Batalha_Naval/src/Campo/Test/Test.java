12c3d9549bd06c73351cbaae4546629fa37aa879
Leon <leondias1989@gmail.com>
2014-04-09 22:16:58 -0300

    Vector Test

--- ALG_M_T3/Batalha_Naval/src/Campo/Test/Test.java
+++ ALG_M_T3/Batalha_Naval/src/Campo/Test/Test.java
@@ -6,12 +6,14 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		CampoView c0 = new CampoView();
-		
-		c0.mostrarCampo();
-		
+		CampoView[] cv = new CampoView[10];
 
-		
+		for (int i = 0; i < cv.length; i++) {
+			cv[i] = new CampoView();
+			cv[i].mostrarCampo();
+			System.out.println("\n");
+		}
+
 	}
 
 }
